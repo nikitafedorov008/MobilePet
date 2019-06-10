5897cd5032015306c767d79998d12b4e84f60cb6
nikitafedorov008 <agent007>
2019-06-10 14:44:06 +0300

    Statistics view on fragment

--- app/src/main/java/ru/com/jetbrainsresearch/ido/StatisticsFragment.java
+++ app/src/main/java/ru/com/jetbrainsresearch/ido/StatisticsFragment.java
@@ -1,17 +1,32 @@
 package ru.com.jetbrainsresearch.ido;
 
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnticipateInterpolator;
+import android.widget.TextView;
+
+import com.hookedonplay.decoviewlib.DecoView;
+import com.hookedonplay.decoviewlib.charts.DecoDrawEffect;
+import com.hookedonplay.decoviewlib.charts.SeriesItem;
+import com.hookedonplay.decoviewlib.events.DecoEvent;
+import com.txusballesteros.widgets.FitChart;
+import com.txusballesteros.widgets.FitChartValue;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class StatisticsFragment extends Fragment {
+
 
 
     public StatisticsFragment() {
@@ -24,7 +39,27 @@ public class StatisticsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_statistics, container, false);
 
+
+        final FitChart fitChart = (FitChart)view.findViewById(R.id.fitChart);
+        fitChart.setMinValue(0f);
+        fitChart.setMaxValue(100f);
+
+        view.findViewById(R.id.add).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Resources resources = getResources();
+                Collection<FitChartValue> values = new ArrayList<>();
+                values.add(new FitChartValue(30f, resources.getColor(R.color.chart_value_1)));
+                values.add(new FitChartValue(20f, resources.getColor(R.color.chart_value_2)));
+                values.add(new FitChartValue(15f, resources.getColor(R.color.chart_value_3)));
+                values.add(new FitChartValue(10f, resources.getColor(R.color.chart_value_4)));
+                fitChart.setValues(values);
+            }
+        });
+
         return view;
     }
+
+
 
 }
